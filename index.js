73cf1a34b00790394bea548f7af3ea97d55b367b
monica-kuppuswamy <mpadmana@uncc.edu>
2018-05-19 06:40:56 -0400

    Adding react router for SPA

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import RestaurantList from './containers/restaurant_list';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+
+    <BrowserRouter>
+      <Route path="/" component = { RestaurantList } />
+    </BrowserRouter>
+
   </Provider>
   , document.querySelector('.container'));
